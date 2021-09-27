be49285e1a6f6b2eb7dc8fb826e1151c7becb5da
Mirzam2 <91127036+Mirzam2@users.noreply.github.com>
2021-09-27 15:32:27 +0300

    Update Ex_5.py

--- Turtle_2/Ex_5.py
+++ Turtle_2/Ex_5.py
@@ -1,15 +1,17 @@
-import turtle
 from random import randint
+import turtle
+turtle.tracer(10, 1)
 
-turtle.speed(10)
 
-n = 50   # кол-о черепах
-t = 100   # кол-о иттераций
+turtle.speed(0)
+
+n = 50   # колво черепах
+t = 1000000
 xb = 300  # границы
 yb = 300
-vmax = 10
+vmax = 50  # во сколько уменьшается скорости
+r = 10  # расстояние , когда отталкиваются
 
-'''Рисование границ'''
 sp = list()
 turtle.penup()
 turtle.goto(xb, yb)
@@ -23,6 +25,8 @@ turtle.hideturtle()
 '''Генерация положения и скоростей частиц газа'''
 pool = [turtle.Turtle() for i in range(n)]
 for unit in pool:
+    unit.resizemode('user')
+    unit.shapesize(0.5, 0.5)
     unit.shape('circle')
     unit.penup()
     unit.goto(randint(-xb, xb), randint(-yb, yb))
@@ -32,9 +36,17 @@ for unit in pool:
 for _ in range(t):
     i = 0
     for unit in pool:
-        if abs(unit.xcor()) >= xb:
+
+        '''Проверка на столкновение с другими частицами'''
+        for j in range(i, n):
+            if ((pool[i].xcor() - pool[j].xcor()) ** 2 + (pool[i].ycor() - pool[j].ycor()) ** 2) <= r ** 2:
+                sp[i][1], sp[j][1] = sp[j][1], sp[i][1]
+                sp[i][0], sp[j][0] = sp[j][0], sp[i][0]
+        '''Проверка на столкновение со стенами'''
+        if abs(unit.xcor() + sp[i][0]) >= xb:
             sp[i][0] = -sp[i][0]
-        if abs(unit.ycor()) >= yb:
+        if abs(unit.ycor() + sp[i][1]) >= yb:
             sp[i][1] = -sp[i][1]
+        '''Движение'''
         unit.goto(unit.xcor() + sp[i][0], unit.ycor() + sp[i][1])
         i += 1
