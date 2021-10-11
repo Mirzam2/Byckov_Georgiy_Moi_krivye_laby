4d9c2f4f10da32c8ac305fa7ed848996bbbe641f
Mirzam2 <666mopsegor@gmail.com>
2021-10-11 17:03:42 +0300

    Всё сделал через классы

--- catch_a_balloon/game.py
+++ catch_a_balloon/game.py
@@ -3,7 +3,7 @@ from pygame.draw import *
 from random import randint, random
 pygame.init()
 
-FPS = 2
+FPS = 30
 size_x, sixe_y = 800, 800
 screen = pygame.display.set_mode((size_x, sixe_y))
 
@@ -16,30 +16,31 @@ CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
-
-def generate_balls(number_of_balls: int, x1, y1, x2, y2):
-    def new_ball():
-        '''рисует новый шарик '''
-        x = randint(x1, x2)
-        y = randint(y1, y2)
-        r = randint(10, 100)
-        vx = random() * 20 - 10
-        vy = random() * 20 - 10
-        color = COLORS[randint(0, 5)]
-        balls.append((color, (x, y), r, vx, vy))
-    global balls
-    balls = list()
-    for i in range(number_of_balls):
-        new_ball()
+number_of_balls = 100
+speed = 10
+ball = list()
 
 
-def vyvod(n):
-    for i in range(n):
-        circle(screen, balls[i][0], balls[i][1], balls[i][2], width=0)
+class Ball:
+    def __init__(self, x1, x2, y1, y2):
+        self.x = randint(x1, x2)
+        self.y = randint(y1, y2)
+        self.r = randint(1, 10)
+        self.vx = random() * speed * 2 - speed
+        self.vy = random() * speed * 2 - speed
+        self.color = COLORS[randint(0, 5)]
+
+    def move(self, t):
+        self.x += self.vx * t
+        self.y += self.vy * t
+
+    def vyvod(self):
+        circle(screen, self.color, (self.x, self.y), self.r)
 
 
-generate_balls(100, 100, 100, 700, 700)
-vyvod(100)
+for i in range(number_of_balls):
+    ball.append(Ball(100, 700, 100, 700))
+    ball[i].vyvod()
 
 pygame.display.update()
 clock = pygame.time.Clock()
@@ -50,8 +51,17 @@ while not finished:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
-
+        elif event.type == pygame.MOUSEBUTTONDOWN and event.button==1:
+            for i in len(ball):
+                if (ball[i].x-event.pos[0] ** 2 + (ball[i].y - event.pos[1]) ** 2) <= ball[i].r**2:
+                   ooooo = ball.pop(i) 
     pygame.display.update()
-    screen.fill(BLACK)
+    screen.fill((255, 255, 255))
+    rect(screen, BLACK, (100, 100, 600, 600))
+
+    for i in range(len(ball)):
+        ball[i].vyvod()
+        ball[i].move(1)
+
 
 pygame.quit()
