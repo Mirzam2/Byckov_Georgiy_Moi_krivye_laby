e6f6ba5253552ef144d25df49c3082526b8195e1
Mirzam2 <666mopsegor@gmail.com>
2021-10-20 00:09:44 +0300

    Сделан вывод таймера на экран Нормально оформлен вывод таблицы результатов Подкоректированны параметры генерации объектов

--- catch_a_balloon/game.py
+++ catch_a_balloon/game.py
@@ -6,7 +6,7 @@ from pygame.draw import *
 from random import randint, random
 pygame.init()
 FPS = 30
-size_x, size_y = 600, 400  # размеры экрана
+size_x, size_y = 1400, 800  # размеры экрана
 wall = 100  # толщина стенок
 screen = pygame.display.set_mode((size_x, size_y))
 
@@ -20,7 +20,7 @@ BLACK = (0, 0, 0)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 my_font = pygame.freetype.SysFont(
     'Times New Roman', 35)  # задание параметров текста
-number_of_unit = 5  # количество объектов
+number_of_unit = 50  # количество объектов
 speed = 10  # максимальная скорость мячей
 price_ball = 1
 price_square = 5
@@ -28,6 +28,7 @@ score = 0  # счётчик пойманных мячей
 unit = list()
 x1, x2, y1, y2 = wall, size_x - wall, wall, size_y - wall
 timer = 0
+deadline = 60
 
 
 class Ball:
@@ -142,17 +143,16 @@ class Square:
 
 
 # генерация массива с юнитами
-number_of_balls = randint(0, number_of_unit)
+number_of_balls = randint(number_of_unit // 3, number_of_unit * 2 // 3)
 number_of_squers = number_of_unit - number_of_balls
 for i in range(number_of_balls):
     unit.append(Ball(wall, size_x - wall, wall, size_y - wall))
 for i in range(number_of_squers):
     unit.append(Square(wall, size_x - wall, wall, size_y - wall))
-
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
-while not finished and timer <= 60 and len(unit) != 0:
+while not finished and timer <= deadline and len(unit) != 0:
     clock.tick(FPS)
     timer += 1 / FPS
     for event in pygame.event.get():
@@ -167,24 +167,52 @@ while not finished and timer <= 60 and len(unit) != 0:
     rect(screen, BLACK, (x1, y1, (x2-x1), (y2-y1)))
     my_font.render_to(screen, (20, 20), "score:" + str(score) + "/" +
                       str(number_of_balls * price_ball + number_of_squers * price_square), BLACK)
+    my_font.render_to(screen, (size_x - wall - 380, 20), "Осталось времени:" +
+                      str(deadline - int(timer)) + "/" + str(deadline), BLACK)
     for i in range(len(unit)):
         unit[i].vyvod()
         unit[i].move(1)
         unit[i].sten(x1, x2, y1, y2, 1)
-print('Enter your name')
-name = input()
+screen.fill(BLACK)
+my_font.render_to(screen, (100, 100), "Введите своё имя:", RED)
+pygame.display.update()
+finished = False
+name = ""
+while not finished:
+    screen.fill(BLACK)
+    my_font.render_to(screen, (100, 100), "Введите своё имя:", RED)
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                finished = True
+            elif event.key == pygame.K_BACKSPACE:
+                name = name[:-1]
+            else:
+                name += pygame.key.name(event.key)
+        my_font.render_to(screen, (100, 140), name, RED)
+        pygame.display.update()
+
 with open(r"D:\Проги\Byckov_Georgiy_Moi_krivye_laby\catch_a_balloon\record_table.json") as f:
     data = json.load(f)
 data[name] = score
 if len(unit) == 0:
     rect(screen, RED, (0, 0, size_x, size_y))
-    my_font.render_to(screen, (20, 20), "your are fucking slave", (255,255,255))
+    my_font.render_to(screen, (20, 20),
+                      name + " - dungeon master", (255, 255, 255))
+else:
+    rect(screen, RED, (0, 0, size_x, size_y))
+    my_font.render_to(screen, (20, 20),
+                      name + " - fucking slave", (255, 255, 255))
 h = 50
 for k, v in data.items():
-    my_font.render_to(screen, (20, h), k + ":=" + str(v), (255,255,255))
+    my_font.render_to(screen, (20, h), k + ":=" + str(v), (255, 255, 255))
     h += 30
-    pygame.display.update()
-    clock.tick(1)
-with open (r"D:\Проги\Byckov_Georgiy_Moi_krivye_laby\catch_a_balloon\record_table.json",'w') as f:
-    json.dump(data,f)
+pygame.display.update()
+finished = False
+while not finished:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+with open(r"D:\Проги\Byckov_Georgiy_Moi_krivye_laby\catch_a_balloon\record_table.json", 'w') as f:
+    json.dump(data, f)
 pygame.quit()
