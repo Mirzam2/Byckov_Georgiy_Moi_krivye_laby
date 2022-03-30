39018552532cefd59c4f646de16b3245546bbd53
Mirzam2 <666mopsegor@gmail.com>
2022-03-31 00:42:16 +0300

    top, push

---
+++ cpp/classwork8/Stack.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+struct Node
+{
+    int field;
+    Node *next;
+};
+
+struct Stack
+{
+    Node *top;
+    Stack()
+    {
+        top = new Node;
+        top = nullptr;
+    }
+
+    void push(int value)
+    {
+
+        Node *temp;
+        temp = new Node;
+        if (top == nullptr)
+        {
+            top = new Node;
+            temp = nullptr;
+        }
+        else
+        {
+            temp->field = top->field;
+            temp->next = top->next;
+        }
+        top->field = value;
+        top->next = temp;
+    }
+    void pop(){
+        Node *temp;
+        temp = new Node;
+        temp = top->next;
+        delete top;
+        top = temp;
+    }
+    friend std::ostream &operator<<(std::ostream &out, Stack &st)
+    {
+        Node *temp = st.top;
+        while (temp != nullptr)
+        {
+            out << temp->field << " ";
+            temp = temp->next;
+        }
+        out << '\n';
+        return out;
+    }
+};
+
+int main()
+{
+    Stack stack;
+    std::cout << stack;
+    stack.push(7);
+    stack.push(50);
+    stack.pop();
+    stack.pop();
+    std::cout << stack;
+}
