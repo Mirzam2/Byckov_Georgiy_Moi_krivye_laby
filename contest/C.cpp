946d279e53067dfe838c666ba8e97d23525c29fe
Mirzam2 <666mopsegor@gmail.com>
2022-04-01 14:43:46 +0300

    comit

---
+++ contest/C.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+int *compress(int **matrix, int n, int m)
+{
+    int *ptr = new int[m * 3];
+    int *temp = &ptr[0];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            int s = 0;
+            if (matrix[i][j] != 0)
+            {
+                s++;
+                *temp = matrix[i][j];
+                temp++;
+            }
+            if (s >3){
+                delete[] matrix;
+                return nullptr;
+            }
+        }
+    }
+    delete[] matrix;
+    return ptr;
+};
+// Здесь ваша реализация функции.
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    int **a = new int *[n];
+    for (int i = 0; i < n; i++)
+        a[i] = new int[m];
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            cin >> a[i][j];
+
+    int *compressed = compress(a, n, m);
+
+    if (!compressed)
+        return 0;
+
+    for (int i = 0; i < n * 3; i++)
+        cout << compressed[i] << " ";
+    cout << endl;
+
+    delete[] compressed;
+    return 0;
+}
