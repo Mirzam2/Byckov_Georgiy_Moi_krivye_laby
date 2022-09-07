86767480f646837be446259d13e32e425b486002
Mirzam2 <666mopsegor@gmail.com>
2022-09-07 23:19:44 +0300

    D?

---
+++ 3_sem_cpp/classwork01/D.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <limits>
+
+int main()
+{
+    std::cout << "type:          size         min_limit         max_limit"  << "\n";
+    std::cout << "char" << ' ' << 8 * sizeof(char) << ' ' << int(std::numeric_limits<char>::min()) << ' ' << int(std::numeric_limits<char>::max()) << "\n";
+    std::cout << "short int" << ' ' << 8 * sizeof(short int) << ' ' << std::numeric_limits<short int>::min() << ' ' << std::numeric_limits<short int>::max() << "\n";
+    std::cout << "int" << ' ' << 8 * sizeof(int) << ' ' << std::numeric_limits<int>::min() << ' ' << std::numeric_limits<int>::max() << "\n";
+    std::cout << "long int" << ' ' << 8 * sizeof(long int) << ' ' << std::numeric_limits<long int>::min() << ' ' << std::numeric_limits<long int>::max() << "\n";
+    std::cout << "float" << ' ' << 8 * sizeof(float) << ' ' << std::numeric_limits<float>::min() << ' ' << std::numeric_limits<float>::max() << "\n";
+    std::cout << "double" << ' ' << 8 * sizeof(double) << ' ' << std::numeric_limits<double>::min() << ' ' << std::numeric_limits<double>::max() << "\n";
+    return 0;
+}
