d0f6d85fa6b8c4cee4646d49c8583396fa290970
Mirzam2 <666mopsegor@gmail.com>
2022-10-01 00:53:24 +0300

    Я сделаль, но только для одной частицы

---
+++ 3_sem_cpp/labs02/graph.py
@@ -0,0 +1,12 @@
+import numpy as np
+import json
+import matplotlib.pyplot as plt
+with open("results.json") as f:
+    data = json.load(f)
+x = np.linspace(100, 1000, 901)
+plt.plot(x, data)
+plt.xlabel(r'N', fontsize=14)
+plt.ylabel(r'P', fontsize=14)
+plt.title(r'$Вероятность от количества испытаний$')
+print("HELP")
+plt.show()
--- 3_sem_cpp/labs02/main.cpp
+++ 3_sem_cpp/labs02/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <set>
 #include <random>
-
+#include <cmath>
+#include <fstream>
 int Max_Size = 1000;
 class SetState
 {
@@ -125,6 +126,27 @@ int main(int argc, const char *argv[])
     DiscreteState d(1);
     SegmentState s(0, 10);
     SetState ss({1, 3, 5, 7, 23, 48, 57, 60, 90, 99});
+    std::ofstream file("results.json"); // Открываем файл для записи
+    file << "[" << '\n';
+    for (int N = 100; N <= 1000; ++N)
+    {
+        //std::default_random_engine rng(time_t());
+        //std::uniform_int_distribution<int> dstr(0, 2000000);
+        float result = 0.0;
+        for (int i = 0; i < 1000; ++i)
+        {
+            ProbabilityTest pt(time_t(), 0, 100, N);
+            result +=pt(ss) / N;
+        }
+        file << result;
+        if (N != 1000)
+        {
+            file << ", ";
+        }
+        //std::cout << result << '\n';
+    }
+    file << "]";
+    file.close();
     ProbabilityTest pt(10, 0, 100, 100000);
     std::cout << pt(d) << std::endl;
     std::cout << pt(s) << std::endl;
